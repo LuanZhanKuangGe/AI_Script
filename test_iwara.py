4bb48b905663b0dbf0644139c7970a487d3d77cd
coderluan <zhoubo616819598@gmail.com>
2026-03-24 22:57:51 +0800

    Delete test_iwara.py

--- test_iwara.py
+++
@@ -1,8 +0,0 @@
-import json
-
-new_iwara_data = []
-with open("iwara_videos.json", "r", encoding="utf8") as fp:
-    new_iwara_data = json.load(fp)
-print(new_iwara_data[0]['id'])
-print(new_iwara_data[0]['user']['username'])
-print(new_iwara_data[0]['user']['name'])
