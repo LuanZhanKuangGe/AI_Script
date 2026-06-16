efa155f6c2af56d22afc3cad527bb51672a1c24f
coderluan <zhoubo616819598@gmail.com>
2026-06-16 22:29:54 +0800

    fix: collectData 改为无参 protected，兼容父类签名

--- rss-bridge/ExampleBridge.php
+++ rss-bridge/ExampleBridge.php
@@ -5,8 +5,11 @@ class ExampleBridge extends BridgeAbstract {
     const DESCRIPTION = '最简单的 RSS-Bridge 自定义例子';
     const MAINTAINER = 'YourName';
 
-    public function collectData($params) {
+    protected function collectData() {
         $html = getSimpleHTMLDOM('https://example.com');
+        if (!$html) {
+            return;
+        }
         foreach ($html->find('h1') as $element) {
             $item = new \Item();
             $item->title = $element->plaintext;
