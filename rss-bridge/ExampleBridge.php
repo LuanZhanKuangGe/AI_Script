174d7a0a73c1aca25c17890946e9d2c0fb48c335
coderluan <zhoubo616819598@gmail.com>
2026-06-16 22:28:31 +0800

    fix: 去掉 collectData 的 array 类型声明以兼容父类

--- rss-bridge/ExampleBridge.php
+++ rss-bridge/ExampleBridge.php
@@ -5,7 +5,7 @@ class ExampleBridge extends BridgeAbstract {
     const DESCRIPTION = '最简单的 RSS-Bridge 自定义例子';
     const MAINTAINER = 'YourName';
 
-    public function collectData(array $params) {
+    public function collectData($params) {
         $html = getSimpleHTMLDOM('https://example.com');
         foreach ($html->find('h1') as $element) {
             $item = new \Item();
