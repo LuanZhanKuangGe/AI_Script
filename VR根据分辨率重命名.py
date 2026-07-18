7d8082a2a9187c17d219989344d20f215c4043e6
coderluan <zhoubo616819598@gmail.com>
2026-07-18 20:37:06 +0800

    add vrcucking support (SLR)

--- VR根据分辨率重命名.py
+++ VR根据分辨率重命名.py
@@ -33,7 +33,7 @@ MONTH_MAP = {
 
 UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 
-SUPPORTED_STUDIOS = {"darkroomvr", "18vr", "babevr", "badoinkvr", "czechvr", "czechvrfetish", "czechvrcasting", "deepinsex", "fuckpassvr", "hamezo", "jimmydraws", "kinky-girls-berlin", "lethalhardcorevr", "littlecapricevr", "lustreality", "migotovr", "milfvr", "no2studiovr", "porncornvr", "povcentralvr", "povr", "realjamvr", "realitylovers", "sexbabesvr", "sexlikereal", "stasyqvr", "tmwvrnet", "virtualrealporn", "virtualtaboo", "vrallure", "vrbangers", "vrconk", "vrcosplayx"}
+SUPPORTED_STUDIOS = {"darkroomvr", "18vr", "babevr", "badoinkvr", "czechvr", "czechvrfetish", "czechvrcasting", "deepinsex", "fuckpassvr", "hamezo", "jimmydraws", "kinky-girls-berlin", "lethalhardcorevr", "littlecapricevr", "lustreality", "migotovr", "milfvr", "no2studiovr", "porncornvr", "povcentralvr", "povr", "realjamvr", "realitylovers", "sexbabesvr", "sexlikereal", "stasyqvr", "tmwvrnet", "virtualrealporn", "virtualtaboo", "vrallure", "vrbangers", "vrconk", "vrcosplayx", "vrcucking"}
 
 FILENAME_RE = re.compile(
     r'^\[(.+?)\]\s*(?:\[(\d{8})\]\s*)?(?:\[(\d+k)\]\s*)?(.+?)\.(mp4|mov)$', re.IGNORECASE)
@@ -397,6 +397,7 @@ STUDIO_FETCHERS = {
     "vrbangers": _vrbangers_info,
     "vrconk": lambda slug: _vrbangers_info(slug, "https://vrconk.com"),
     "vrcosplayx": _vrcosplayx_info,
+    "vrcucking": _slr_info,
 }
 
 
